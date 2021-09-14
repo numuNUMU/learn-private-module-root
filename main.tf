984e85af62bbaf5247e04e02015be939b5afc78c
numuNUMU <34230905+numuNUMU@users.noreply.github.com>
2021-09-14 17:33:29 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 module "module-week4" {
   source  = "app.terraform.io/aws-kr-group/module-week4/aws"
-  # name   = var.name
-  # region = var.region
-  # prefix = var.prefix
+  name   = var.name
+  region = var.region
+  prefix = var.prefix
   version = "1.0.0"
 }
