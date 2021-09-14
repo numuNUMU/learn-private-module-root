32cb54afc99e88857b7e9cbb2832d2fcfa82409b
numuNUMU <34230905+numuNUMU@users.noreply.github.com>
2021-09-14 17:28:41 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 module "module-week4" {
   source  = "app.terraform.io/aws-kr-group/module-week4/aws"
-  name   = var.name
-  region = var.region
-  prefix = var.prefix
+  # name   = var.name
+  # region = var.region
+  # prefix = var.prefix
   version = "1.0.0"
 }
